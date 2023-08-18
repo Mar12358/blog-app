352d92d9cd6a359156d1578bd1f6974d84ccf607
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-18 14:45:03 -0300

    Fix Tests for Post, Add update_post_counter test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -50,16 +50,29 @@ RSpec.describe Post, type: :model do
       @user = User.new(name: 'Tomas', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
       @user.save
       @post = Post.new(author: @user, title: 'Hello', text: 'This is my first post')
-      @like = Like.new(post: @post, author: @user )
       @post.save
-      @like.save
     end
 
 
-    it 'update_likes_counter counts automatically when creating a new Like (as a callback function)' do
-      expect(@like.post.likes_counter).to eq(1)
-      second_like = Like.create(post: @post, author: @user )
-      expect(@like.post.likes_counter).to eq(2)
+    it 'update_posts_counter counts automatically when creating a new Like (as a callback function)' do
+      expect(@user.posts_counter).to eq(1)
+      second_post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
+      expect(@user.posts_counter).to eq(2)
+    end
+
+    describe 'recent_comments Method' do
+      it 'returns the five most recent comments' do
+        comment1 = Comment.create(post: @post, author: @user, text: 'Hi Tom! 1', created_at: 7.day.ago )
+        comment2 = Comment.create(post: @post, author: @user, text: 'Hi Tom! 2', created_at: 6.day.ago )
+        comment3 = Comment.create(post: @post, author: @user, text: 'Hi Tom! 3', created_at: 5.day.ago )
+        comment4 = Comment.create(post: @post, author: @user, text: 'Hi Tom! 4', created_at: 4.day.ago )
+        comment5 = Comment.create(post: @post, author: @user, text: 'Hi Tom! 5', created_at: 3.day.ago )
+        comment6 = Comment.create(post: @post, author: @user, text: 'Hi Tom! 6', created_at: 2.day.ago )
+        comment7 = Comment.create(post: @post, author: @user, text: 'Hi Tom! 7', created_at: 1.day.ago )
+        comment8 = Comment.create(post: @post, author: @user, text: 'Hi Tom! 8' )
+  
+        expect(@post.recent_comments).to eq([comment8, comment7, comment6, comment5, comment4])
+      end
     end
   end
 
