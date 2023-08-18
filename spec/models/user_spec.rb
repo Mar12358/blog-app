afa9a7dcfd6d579484efa0bf4223b9bcba1c649c
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-18 13:49:39 -0300

    Add test for User methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,5 +25,19 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe 'recent_posts Method' do
+    it 'returns the three most recent posts' do
+      user = User.new(name: 'Martin')
+      user.save
+      post1 = Post.create(author: user, title: 'Hello', text: 'This is my first post', created_at: 4.day.ago)
+      post2 = Post.create(author: user, title: 'Hello', text: 'This is my second post', created_at: 3.day.ago)
+      post3 = Post.create(author: user, title: 'Hello', text: 'This is my third post', created_at: 2.day.ago)
+      post4 = Post.create(author: user, title: 'Hello', text: 'This is my fourth post', created_at: 1.day.ago)
+      post5 = Post.create(author: user, title: 'Hello', text: 'This is my fifth post')
+
+      expect(user.recent_posts).to eq([post5, post4, post3])
+    end
+  end
+
 
 end
