b1cf499872ee4e035df38301749b1581b47b66f0
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-18 13:48:56 -0300

    Fix after_destroy callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   after_create :update_posts_counter
-  after_delete :update_posts_counter
+  after_destroy :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250, too_long: '%<count>s characters is the maximum allowed' }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
