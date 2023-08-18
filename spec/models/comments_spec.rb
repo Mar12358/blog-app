7e12161662a4afa884dcd7273bc982b5d22f9f48
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-18 14:12:38 -0300

    Cleanup code

--- spec/models/comments_spec.rb
+++ spec/models/comments_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  describe 'validations' do
+  describe 'comments_counter method' do
     
     before(:each) do
       @user = User.new(name: 'Tomas', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
@@ -12,28 +12,13 @@ RSpec.describe Comment, type: :model do
       @comment.save
     end
 
-    it 'comments_counter method works (as a callback function)' do
+    it 'counts automatically when creating a new Comment (as a callback function)' do
       expect(@comment.post.comments_counter).to eq(1)
       second_comment = Comment.create(post: @post, author: @user, text: 'Hi Tom!' )
       expect(@comment.post.comments_counter).to eq(2)
-
     end
     
 
-  end
-
-  describe 'recent_posts Method' do
-    it 'returns the three most recent posts' do
-      user = User.new(name: 'Martin')
-      user.save
-      post1 = Post.create(author: user, title: 'Hello', text: 'This is my first post', created_at: 4.day.ago)
-      post2 = Post.create(author: user, title: 'Hello', text: 'This is my second post', created_at: 3.day.ago)
-      post3 = Post.create(author: user, title: 'Hello', text: 'This is my third post', created_at: 2.day.ago)
-      post4 = Post.create(author: user, title: 'Hello', text: 'This is my fourth post', created_at: 1.day.ago)
-      post5 = Post.create(author: user, title: 'Hello', text: 'This is my fifth post')
-
-      expect(user.recent_posts).to eq([post5, post4, post3])
-    end
   end
 
 
