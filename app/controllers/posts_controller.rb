94880345911fc85d741d056ade65be2317135563
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-23 19:38:52 -0300

    Fix Linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @page_title = "Post Index Page"
+    @page_title = 'Post Index Page'
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: @user.id)
   end
+
   def show
-    @page_title = "Post Show Page"
+    @page_title = 'Post Show Page'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @page_title = "User Index Page"
+    @page_title = 'User Index Page'
     @users = User.all
   end
+
   def show
-    @page_title = "User Show Page"
+    @page_title = 'User Show Page'
     @user = User.find(params[:id])
     @posts = Post.where(author_id: @user.id)
   end
