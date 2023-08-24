23b53c3f03404b7c4fc2b746e794ed32eda37f1d
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-24 16:45:20 -0300

    Define actions in PostControllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_user
+
   def index
     @page_title = 'Post Index Page'
     @user = User.find(params[:user_id])
@@ -7,7 +9,28 @@ class PostsController < ApplicationController
 
   def show
     @page_title = 'Post Show Page'
-    @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+  end
+
+  def new
+    
+    @post = @user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
