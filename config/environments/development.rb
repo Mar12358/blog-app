5ea65a781fa1f0212f8f9b13873952df0dccb6b8
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-29 01:00:08 -0300

    Remove bullet config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,21 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.after_initialize do
-  Bullet.enable = true
-  Bullet.sentry = true
-  Bullet.alert = true
-  Bullet.bullet_logger = true
-  Bullet.console = true
-  Bullet.rails_logger = true
-  Bullet.honeybadger = true
-  Bullet.bugsnag = true
-  Bullet.appsignal = true
-  Bullet.airbrake = true
-  Bullet.rollbar = true
-  Bullet.add_footer = true
-  Bullet.skip_html_injection = false 
-end
 
   # Settings specified here will take precedence over those in config/application.rb.
 
