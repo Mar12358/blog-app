46c40ac0d91f8c77a531a13d9a57dcf6e4610d82
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-30 00:31:17 -0300

    Add permission to read Posts if user.present?

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     return unless user.present?
-
+    can :read, Post
     can :manage, Post, author_id: user.id # if the user is logged in can manage it's own posts
     can :manage, Comment, author_id: user.id # logged in users can also create comments
     return unless user.admin?
