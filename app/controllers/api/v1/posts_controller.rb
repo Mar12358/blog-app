20d98d8f7d4a478ba8a2a65f23b9ec483f965f5f
Luiscarlosvd <lc26cr7@gmail.com>
2023-08-31 11:14:48 -0400

    Add new PostsController for a user posts in json

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::PostsController < ApplicationController
+    def index
+        @user = User.includes(:posts).find(params[:user_id])
+
+        respond_to do |format|
+            format.json { render :json => @user.posts }
+        end
+    end
+end
