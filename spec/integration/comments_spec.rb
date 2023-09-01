6426ba9bc3ce41ca5207a2852daf211b1f106d5a
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-01 12:45:49 -0300

    Fix Linters

--- spec/integration/comments_spec.rb
+++ spec/integration/comments_spec.rb
@@ -1,84 +1,93 @@
 require 'swagger_helper'
 
 describe 'Comments API' do
-
-
   path '/api/v1/users/{user_id}/posts/{post_id}/comments' do
-
     post 'Creates a Comment' do
       tags 'Comments'
       consumes 'application/json', 'application/xml'
       parameter name: :pet, in: :body, schema: {
         type: :object,
         properties: {
-          text: { type: :string },
+          text: { type: :string }
         },
-        required: [ 'id', 'author_id', 'post_id', 'text' ]
+        required: %w[id author_id post_id text]
       }
-      let(:user_id) { User.create(
-        name: 'Test User',
-        bio: 'Test Bio',
-        email: 'test@example.com',
-        role: nil
-      ).id }  
-      let(:post_id) {Post.create(
-        author_id: user.id,
-        title: 'Test Post Title',
-        text: 'Test Text Post',
-      ).id }
+      let(:user_id) do
+        User.create(
+          name: 'Test User',
+          bio: 'Test Bio',
+          email: 'test@example.com',
+          role: nil
+        ).id
+      end
+      let(:post_id) do
+        Post.create(
+          author_id: user.id,
+          title: 'Test Post Title',
+          text: 'Test Text Post'
+        ).id
+      end
       response '201', 'Comment created' do
-        
-        let(:comment) { Comment.create(
-          author_id: user_id,
-          post_id: post_id,
-          text: 'Test Comment') }
+        let(:comment) do
+          Comment.create(
+            author_id: user_id,
+            post_id:,
+            text: 'Test Comment'
+          )
+        end
         run_test!
       end
 
       response '422', 'invalid request' do
-        let(:comment) { Comment.create(
-          author_id: user_id,
-          post_id: post_id,
-          text: '') }
+        let(:comment) do
+          Comment.create(
+            author_id: user_id,
+            post_id:,
+            text: ''
+          )
+        end
         run_test!
       end
     end
   end
 
-
   path '/api/v1/users/{user_id}/posts/{post_id}/comments' do
     get 'Retrieves all Comments for a specific Post' do
       tags 'Comments'
       produces 'application/json'
-  
+
       response '200', 'Users found' do
         schema type: :array,
-          items: {
-            type: :object,
-            properties: {
-              id: { type: :integer },
-              text: { type: :string },
-              author_id: { type: :integer },
-              post_id: { type: :integer },
-              created_at: { type: :string, format: 'date-time' },
-              updated_at: { type: :string, format: 'date-time' }
-            },
-            required: ['id', 'author_id', 'post_id', 'text', 'created_at', 'updated_at']
-          }
-        let(:user_id) { User.create(
-          name: 'Test User',
-          bio: 'Test Bio',
-          email: 'test@example.com',
-          role: nil
-        ).id }  
-        let(:post_id) {Post.create(
-          author_id: user.id,
-          title: 'Test Post Title',
-          text: 'Test Text Post',
-        ).id }
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   text: { type: :string },
+                   author_id: { type: :integer },
+                   post_id: { type: :integer },
+                   created_at: { type: :string, format: 'date-time' },
+                   updated_at: { type: :string, format: 'date-time' }
+                 },
+                 required: %w[id author_id post_id text created_at updated_at]
+               }
+        let(:user_id) do
+          User.create(
+            name: 'Test User',
+            bio: 'Test Bio',
+            email: 'test@example.com',
+            role: nil
+          ).id
+        end
+        let(:post_id) do
+          Post.create(
+            author_id: user.id,
+            title: 'Test Post Title',
+            text: 'Test Text Post'
+          ).id
+        end
         run_test!
       end
-  
+
       response '404', 'Users not found' do
         run_test!
       end
--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -15,30 +15,32 @@ describe 'Users API' do
     get 'Retrieves all Users' do
       tags 'Users'
       produces 'application/json'
-  
+
       response '200', 'Users found' do
         schema type: :array,
-          items: {
-            type: :object,
-            properties: {
-              id: { type: :integer },
-              name: { type: :string },
-              bio: { type: :string },
-              email: { type: :string },
-              role: { type: %i[string null] }
-            },
-            required: ['id', 'name', 'bio', 'email']
-          }
-        let!(:user) { User.create(
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   name: { type: :string },
+                   bio: { type: :string },
+                   email: { type: :string },
+                   role: { type: %i[string null] }
+                 },
+                 required: %w[id name bio email]
+               }
+        let!(:user) do
+          User.create(
             name: 'Test User',
             bio: 'Test Bio',
             email: 'test@example.com',
             role: nil
-        ) }  
+          )
+        end
 
         run_test!
       end
-  
+
       response '404', 'Users not found' do
         run_test!
       end
